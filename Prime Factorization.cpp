88a39af6dcdfba61f45587eb64360753096f8c04
Anik Roy <136085433+EtheSonX082531@users.noreply.github.com>
2025-04-01 16:16:52 +0600

    Prime Factorization.cpp

--- Prime Factorization.cpp
+++ Prime Factorization.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int n,x;
     cout << "Enter a number:" << endl;
     cin>>n;
+    x=n;
     list<int>l;
     for(int i=2; i<=sqrt(n); i++) {
         if(n%i==0) {
@@ -18,9 +19,14 @@ int main()
     if(n!=1) {
         l.push_back(n);
     }
+    // with design printing
     cout << "Prime Factorization:" <<endl;
-    for(int i:l) {
-        cout<<i<<" ";
+    for(auto i=l.begin(); i!=l.end(); i++) {
+        cout<<(*i);
+        if(next(i)!=l.end()) {
+            cout<<"×";
+        }
     }
+    cout<<"="<<x<<endl;
     return 0;
 }
